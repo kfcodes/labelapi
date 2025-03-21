0944a6c80aa2f738a6b9021fddcaf91290d9c7b9
kfcodes <+kfcodes@users.noreply.github.com>
2025-03-21 16:32:56 +0000

    Added box label zpl logic module

--- app/data_controller_layer/box_label_controller.py
+++ app/data_controller_layer/box_label_controller.py
@@ -1,19 +1,6 @@
-from physical_layer.data_access_layer.read_database_functions import get_label_data, read_to_list_index
-from business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.small_type_1_box_label import create_small_label_data,  create_small_label_outline
-from business_logic_layer.external_module_controllers.zpl_logic.pallet_logic.generic_pallet_label import create_pallet_label_outline, create_pallet_label_data, add_products_to_label
-from business_logic_layer.external_module_controllers.zpl_logic.internal_logic.internal_pallet_label import create_blank_label_outline
-from business_logic_layer.external_module_controllers.print_logic.print_zpl import print_small_label, print_large_label, print_specific_label
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.large_type_1_box_label as label_type_1
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.large_type_2_box_label as label_type_2
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.large_type_3_box_label as label_type_3
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.large_type_4_box_label as label_type_4
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.large_type_5_box_label as label_type_5
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.large_type_6_box_label as label_type_6
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.small_type_2_box_label as type_2_label
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.small_type_3_box_label as type_3_label
-import business_logic_layer.external_module_controllers.zpl_logic.product_box_logic.small_type_4_box_label as type_4_label
-import pprint
-from physical_layer.data_access_layer.write_database_functions import update_pallet
+from db_access_layer.read_db import read_db
+from external_module_controller_layer.zpl_logic.pallet_label import create_box_label_zpl
+from external_module_controller_layer.printer_connection_logic.zpl_printer_logic import label_printer_connection
 
 import os
 from dotenv import load_dotenv
@@ -21,7 +8,7 @@ load_dotenv(".env")
 
 async def print_large_product_label(id, quantity, quantity_in_a_box, exp):
     try:
-        label_info = read_to_list_index(f"{os.getenv('PRODUCTIONLABELINFO')}{id}")
+        label_info = read_db(f"{os.getenv('PRODUCTIONLABELINFO')}{id}")
         label_type = int(label_info[0][f'{os.getenv("LABELFIELD20")}'])
         label_info = label_info[0]
 
