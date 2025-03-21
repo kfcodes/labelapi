c3446612e2b0b1023fc16e644395d5f6c9c3f18b
kfcodes <+kfcodes@users.noreply.github.com>
2025-03-21 10:00:30 +0000

    fixed elif error

--- app/data_controller_layer/pallet_label.py
+++ app/data_controller_layer/pallet_label.py
@@ -37,27 +37,28 @@ async def upload_pallet_label_data_to_printers():
         print("Pallet label structure could not be uploaded due to: \n", ex)
 
 async def main_pallet_label_function(printer_id, pallet_id):
+    print(printer_id)
+    print(type(printer_id))
     try:
         # setting the printer variables if no printer is stated use default/3rd printer
-        if printer_id == "s":
-            printer_address = os.getenv("L1SID")
-            printer_port = int(os.getenv("L1SP"))
         if printer_id == "c":
             printer_address = os.getenv("L1CID")
             printer_port = int(os.getenv("L1CP"))
+        elif printer_id == "s":
+            printer_address = os.getenv("L1SID")
+            printer_port = int(os.getenv("L1SP"))
         else:
+            print("using else printer")
             printer_address = os.getenv("L2SID")
             printer_port = int(os.getenv("L2SP"))
 
         # get the label summary information and type id from the database
         label_summary_info = read_db(str(f"{os.getenv('PALLETSUMMARY')}").format(int(pallet_id)))
         label_summary_info = label_summary_info[0]
-        # print(label_summary_info)
 
         # label_type_info = read_to_list_index(str(f"{os.getenv('PALLETLABELTYPE')}").format(int(pallet_id)))
         label_type_info = read_db(str(f"{os.getenv('PALLETLABELTYPE')}"))
         label_structure_name = f"{label_type_info[0]['pallet_label_name']}"
-        # print(label_type_info)
         # setting the label structure statically for now
         label_structure_name = "PALSTD1"
 
@@ -71,16 +72,14 @@ async def main_pallet_label_function(printer_id, pallet_id):
         else:
             extra_info = standard_pallet_label_extra_information(pallet_id)
 
-        # print(len(extra_info))
         # create the zpl string with the pallet information
         pallet_label_zpl = create_pallet_label_zpl(label_structure_name, label_summary_info, extra_info)
-        # print(pallet_label_zpl)
 
         # send the zpl string with the printer info to the print function
         response = label_printer_connection(pallet_label_zpl, printer_address, printer_port)
 
         # update the pallet in the database to add it to packing list
-        update_pallet_packing_list(id);
+        # update_pallet_packing_list(id);
 
         return response
 
@@ -114,4 +113,4 @@ def standard_pallet_label_with_product_skus_extra_information(pallet_id):
     # get the pallet item information from DB
     pallet_contents = read_db(f"{os.getenv('GETPRODUCTSONPALLET1')} {int(pallet_id)} {os.getenv('GETPRODUCTSONPALLET2')}")
     pallet_contents = tuple(pallet_contents.values())
-    return [pallet_contents];
+    return pallet_contents;
