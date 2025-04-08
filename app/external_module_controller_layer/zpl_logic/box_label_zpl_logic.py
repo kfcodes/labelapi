b2a8af5eb56550fd3857a8c61631e13b479e2ce0
kfcodes <+kfcodes@users.noreply.github.com>
2025-04-08 23:15:00 +0100

    Fixed the logic for printing the pallet labels from the system

--- app/external_module_controller_layer/zpl_logic/box_label_zpl_logic.py
+++ app/external_module_controller_layer/zpl_logic/box_label_zpl_logic.py
@@ -2,26 +2,33 @@ import os
 from dotenv import load_dotenv
 load_dotenv(".env")
 
-def create_box_label_zpl(label_data):
+def create_box_label_zpl(label_data, qty):
     try:
         # Destructure the label data into the required variables
-        name = {label_data['label_structure_name']}
-        item = {label_data['desctription']}
-        sku = {label_data['sku']}
-        lot = {label_data['lot']}
-        bn = {label_data['batch']}
-        bbe = {label_data['bbe']}
-        bc = {label_data['box_barcode']}
-        qty = {label_data['quantity']}
-        exp = {label_data['exp']}
-
+       name = {label_data['label_structure_name']}
+       item = {label_data['desctription']}
+       sku = {label_data['sku']}
+       bx_qty = {label_data['quantity']}
+       bc = {label_data['box_barcode']}
+       lot = {label_data['lot']}
+       bn = {label_data['batch']}
+       bbe = {label_data['bbe']}
+       exp = {label_data['exp']}
 
         # if else statement to determin the label structure
         # get pallet structure name then apply the applicable variables
         zpl = f"""
            ^XA
-           ^XFE:{label_type}.ZPL^FS
-           {pallet_information}
+           ^XFE:{name}.ZPL^FS
+           ^PQ{qty},10,1,Y
+            ^FN1^FD{item}^FS
+            ^FN2^FD{sku}^FS
+            ^FN3^FD{bx_qty}^FS
+            ^FN4^FD{bc}^FS
+            ^FN5^FD{lot}^FS
+            ^FN6^FD{bn}^FS
+            ^FN7^FD{bbe}^FS
+            ^FN8^FD{exp}^FS
            ^XZ"""
 
         # then make the label string with the XA and XZ commands
