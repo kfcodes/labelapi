7ec158b75c00bfac2524bc88a250a0be512667b2
kfcodes <+kfcodes@users.noreply.github.com>
2025-04-10 23:27:27 +0100

    Updated the pallet label data controller module to work with the new printer data structure

--- app/data_controller_layer/pallet_label_controller.py
+++ app/data_controller_layer/pallet_label_controller.py
@@ -1,30 +1,19 @@
-from db_access_layer.read_db import read_db
-from external_module_controller_layer.zpl_logic.pallet_label_zpl_logic import create_pallet_label_zpl
-from external_module_controller_layer.printer_connection_logic.zpl_printer_logic import label_printer_connection
+from db_access_layer.read_db import *
+from external_module_controller_layer.zpl_logic.pallet_label_zpl_logic import *
+from external_module_controller_layer.printer_connection_logic.zpl_printer_logic import *
+from external_module_controller_layer.printer_connection_logic.zpl_printer_logic import *
 from db_access_layer.write_db import update_pallet_packing_list
+from external_module_controller_layer.printer_connection_logic.zpl_printer_logic import *
 
 import os
 from dotenv import load_dotenv
 load_dotenv("env/pallet_label.env")
 
-async def main_pallet_label_function(printer_id, pallet_id):
+async def main_pallet_label_function(printer, pallet_id):
     try:
-        # setting the printer variables if no printer is stated use default/3rd printer
-        if printer_id == "c":
-            printer_address = os.getenv("L1CID")
-            printer_port = int(os.getenv("L1CP"))
-        elif printer_id == "s":
-            printer_address = os.getenv("L1SID")
-            printer_port = int(os.getenv("L1SP"))
-        else:
-            print("using else printer")
-            printer_address = os.getenv("L2SID")
-            printer_port = int(os.getenv("L2SP"))
-
         # get the label summary information and type id from the database
         label_summary_info = read_db(str(f"{os.getenv('PALLETSUMMARY')}").format(int(pallet_id)))
         label_summary_info = label_summary_info[0]
-
         # label_type_info = read_to_list_index(str(f"{os.getenv('PALLETLABELTYPE')}").format(int(pallet_id)))
         label_type_info = read_db(str(f"{os.getenv('PALLETLABELTYPE')}"))
         label_structure_name = f"{label_type_info[0]['pallet_label_name']}"
@@ -43,15 +32,12 @@ async def main_pallet_label_function(printer_id, pallet_id):
 
         # create the zpl string with the pallet information
         pallet_label_zpl = create_pallet_label_zpl(label_structure_name, label_summary_info, extra_info)
-
         # send the zpl string with the printer info to the print function
-        response = label_printer_connection(pallet_label_zpl, printer_address, printer_port)
+        response = label_printer_connection(pallet_label_zpl, printer['ip'] , printer['port'] )
 
         # update the pallet in the database to add it to packing list
-        update_pallet_packing_list(pallet_id)
-
+        # update_pallet_packing_list(pallet_id, site)
         return response
-
     except Exception as ex:
         print("Pallet label could not be created due to: \n", ex)
 
@@ -68,9 +54,7 @@ async def print_blank_pallet_labels(printer_id):
             print("using else printer")
             printer_address = os.getenv("L2SID")
             printer_port = int(os.getenv("L2SP"))
-
         label_structure_name = "PALBLNK"
-
         label_zpl = create_pallet_label_zpl(label_structure_name)
         response = label_printer_connection(label_zpl, printer_address, printer_port)
         return response
@@ -90,7 +74,6 @@ async def print_combined_pallet_label(data, printer_id):
             print("using else printer")
             printer_address = os.getenv("L2SID")
             printer_port = int(os.getenv("L2SP"))
-
         label_zpl = create_pallet_label_zpl(label_structure_name)
         response = label_printer_connection(label_zpl, printer_address, printer_port)
         return response
@@ -102,41 +85,24 @@ def standard_pallet_label_extra_information(pallet_id):
     # get the pallet item information from DB
     pallet_contents = read_db(f"{os.getenv('GETPRODUCTSONPALLET1')} {int(pallet_id)} {os.getenv('GETPRODUCTSONPALLET2')}")
     pallet_contents = tuple(pallet_contents.values())
-    return pallet_contents;
+    return pallet_contents
 
 def standard_pallet_label_with_product_skus_extra_information(pallet_id):
     # get the pallet item information from DB
     pallet_contents = read_db(f"{os.getenv('GETPRODUCTSONPALLET1')} {int(pallet_id)} {os.getenv('GETPRODUCTSONPALLET2')}")
     pallet_contents = tuple(pallet_contents.values())
-    return pallet_contents;
+    return pallet_contents
 
-async def upload_pallet_label_data_to_printers():
+async def upload_pallet_label_data_to_printers(printers):
     try:
-        # load the label structures into variable
-        label_structures = str(f"{os.getenv('PALLETLABELSTRUCTURES')}")
-
-        # Loop over all printers and send the label information
-        # for each printer in printers;
-        #     send the label_structure
-
+        stored_label_structures = str(f"{os.getenv('PALLETLABELSTRUCTURES')}")
         response = ""
-        # for now just hardocoding the labelprinters
-        printer_address = os.getenv("L1SID")
-        printer_port = int(os.getenv("L1SP"))
-        printer_response = label_printer_connection(label_structures, printer_address, printer_port)
-        response += "printer_response /n"
-        response += "/n"
-        printer_address = os.getenv("L1CID")
-        printer_port = int(os.getenv("L1CP"))
-        printer_response = label_printer_connection(label_structures, printer_address, printer_port)
-        response += "printer_response /n"
-        response += "/n"
-        printer_address = os.getenv("L2SID")
-        printer_port = int(os.getenv("L2SP"))
-        printer_response = label_printer_connection(label_structures, printer_address, printer_port)
-        response += "printer_response /n"
-        response += "/n"
+        for printer in printers:
+            printer_ip = printer["ip"]
+            printer_port = printer["port"]
 
+            printer_response = label_printer_connection(stored_label_structures, printer_ip, printer_port)
+            response += f"{printer_response}\n\n"
         return response
     except Exception as ex:
         print("Pallet label structure could not be uploaded due to: \n", ex)
--- app/routes_layer/pallet_label_routes.py
+++ app/routes_layer/pallet_label_routes.py
@@ -7,14 +7,14 @@ pallet_label_router = APIRouter()
 @pallet_label_router.get("/pallet_label/{pallet_id}")
 async def print_pallet_label(pallet_id: int, request: Request):
     printers = get_printers_on_site(request)
-    response = await main_pallet_label_function(printers['pallet_label_printer'], pallet_id)
+    response = await main_pallet_label_function(printers['misc']['pallet_label_printer'], pallet_id)
     return response
 
 @pallet_label_router.post("/stacked_pallet_label")
 async def print_large_combined_label_function(data: Request):
     json_data =  await data.json()
     printers = get_printers_on_site(data)
-    response = await print_combined_pallet_label(json_data, printers['pallet_label_printer'])
+    response = await print_combined_pallet_label(json_data, printers['misc']['pallet_label_printer'])
     return response
 
 @pallet_label_router.post("/sync_pallet_label_structures")
