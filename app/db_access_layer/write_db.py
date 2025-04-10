cfb466acedfb3a146d5abb98f5956725dbe2f085
kfcodes <+kfcodes@users.noreply.github.com>
2025-04-10 23:15:04 +0100

    Updated the routes layer to select the pallet label printer automatically

--- app/db_access_layer/write_db.py
+++ app/db_access_layer/write_db.py
@@ -1,13 +1,13 @@
 from db_access_layer.db_connection import db
 
-def update_pallet_packing_list(id):
+def update_pallet_packing_list(palletid, site):
     try:
-        print(f"palleid passed to function: {id}")
+        print(f"palleid passed to function: {palletid}")
         connection = db().raw_connection()
         cursor = connection.cursor()
-        cursor.execute(f'Call UpdateOrInsertPackingList({id})');
+        cursor.execute(f'Call UpdateOrInsertPackingList({palletid},{site})')
         cursor.close()
         connection.commit()
-        print("Called the stored function")
+        print("Called the stored function to create or set packing list")
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
--- app/routes_layer/pallet_label_routes.py
+++ app/routes_layer/pallet_label_routes.py
@@ -1,20 +1,20 @@
 from fastapi import APIRouter, Request
 from data_controller_layer.pallet_label_controller import *
-from data_controller_layer.printer_data_controller import *
+from data_controller_layer.printer_data_controller import get_printers_on_site, get_all_printer_connections
 
 pallet_label_router = APIRouter()
 
-@pallet_label_router.get("/pallet_label/{printer_id}/{pallet_id}")
-async def print_pallet_label(printer_id: str, pallet_id: int, request: Request):
-    printers = get_printers_on_site(request);
-    response = await main_pallet_label_function(printers, pallet_id)
+@pallet_label_router.get("/pallet_label/{pallet_id}")
+async def print_pallet_label(pallet_id: int, request: Request):
+    printers = get_printers_on_site(request)
+    response = await main_pallet_label_function(printers['pallet_label_printer'], pallet_id)
     return response
 
-@pallet_label_router.post("/stacked_pallet_label/{printer_id}")
-async def print_large_combined_label_function(data: Request, printer_id: str):
+@pallet_label_router.post("/stacked_pallet_label")
+async def print_large_combined_label_function(data: Request):
     json_data =  await data.json()
     printers = get_printers_on_site(data)
-    response = await print_combined_pallet_label(json_data, printer_id)
+    response = await print_combined_pallet_label(json_data, printers['pallet_label_printer'])
     return response
 
 @pallet_label_router.post("/sync_pallet_label_structures")
