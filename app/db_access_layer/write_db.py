ddb4a32b77a3a9b3ecd87728042d6366834ea6f8
kfcodes <+kfcodes@users.noreply.github.com>
2025-04-13 22:09:05 +0100

    Updated structure for writing the packing list to the DB

--- app/db_access_layer/write_db.py
+++ app/db_access_layer/write_db.py
@@ -3,9 +3,11 @@ from db_access_layer.db_connection import db
 def update_pallet_packing_list(palletid, site):
     try:
         print(f"palleid passed to function: {palletid}")
+        print(f"site passed to function: {site}")
+        print(f"Call UpdateOrInsertPackingList({palletid},'{str(site)}')")
         connection = db().raw_connection()
         cursor = connection.cursor()
-        cursor.execute(f'Call UpdateOrInsertPackingList({palletid},{site})')
+        cursor.execute(f"Call UpdateOrInsertPackingList({palletid},'{str(site)}')")
         cursor.close()
         connection.commit()
         print("Called the stored function to create or set packing list")
