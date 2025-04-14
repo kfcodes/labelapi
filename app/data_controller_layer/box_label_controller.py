158e279074790b4aca3433052d11eb655c472057
kfcodes <+kfcodes@users.noreply.github.com>
2025-04-14 16:55:35 +0100

    WIP box lable data information controller layer

--- app/data_controller_layer/box_label_controller.py
+++ app/data_controller_layer/box_label_controller.py
@@ -7,70 +7,31 @@ import os
 from dotenv import load_dotenv
 load_dotenv("env/box_label.env")
 
-async def main_print_box_label_function(product_id, quantity, printer_id):
+async def main_print_box_label_function(unique_id, quantity, printer):
     try:
         # Get the unique information from the db including the label structure name
-        unique_box_detals = unique_box_detals[0] = read_db(f"{os.getenv('PRODUCTIONLABELINFO')}{product_id}")
-
-        # Determine which printer group to use
-        if printer_id == "c":
-            big_printer_id = os.getenv("L1LCID")
-            big_printer_com = int(os.getenv("L1LCP"))
-            small_printer_id = os.getenv("L1SCID")
-            small_printer_com = int(os.getenv("L1SCP"))
-        elif printer_id == "s":
-            big_printer_id = os.getenv("L1LSID")
-            big_printer_com = int(os.getenv("L1LSP"))
-            small_printer_id = os.getenv("L1SSID")
-            small_printer_com = int(os.getenv("L1SSP"))
-        else:
-            print("using fallback printer")
-            big_printer_id = os.getenv("L2LSID")
-            big_printer_com = int(os.getenv("L2LSP"))
-            small_printer_id = os.getenv("L2SSID")
-            small_printer_com = int(os.getenv("L2SSP"))
-
+        box_label_info = box_label_info[0] = read_db(f"{os.getenv('PRODUCTIONLABELINFO')}{unique_id}")
         # This function determines which printer to use big/small and label structure
-        label_data_string = create_box_label_zpl(unique_box_detals, quantity)
-
-        if int(unique_box_detals['label_size']) == 1:
-            response = label_printer_connection(label_data_string, big_printer_id, big_printer_com)
+        zpl_string = create_box_label_zpl(box_label_info, quantity)
+        # Determine label size and select large or small printer
+        if int(box_label_info['label_size']) == 1:
+            response = label_printer_connection(zpl_string, printer['large']['ip'] , printer['large']['port'] )
         else:
-            response = label_printer_connection(label_data_string, small_printer_id, small_printer_com)
-
+            response = label_printer_connection(zpl_string, printer['small']['ip'] , printer['small']['port'] )
         return response
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-
-
-async def upload_box_label_structures_to_printers():
+async def upload_box_label_structures_to_printers(printers):
     try:
         # load the label structures into variable
-        label_structures = str(f"{os.getenv('PALLETLABELSTRUCTURES')}")
-
+        box_label_structures = str(f"{os.getenv('BOXLABELSTRUCTURES')}")
         response = ""
-        # for now just hardocoding the labelprinters
-        printer_address = os.getenv("L1SID")
-        printer_port = int(os.getenv("L1SP"))
-            big_printer_id = os.getenv("L1LCID")
-            big_printer_com = int(os.getenv("L1LCP"))
-            printer_address = os.getenv("L1SCID")
-            printer_port = int(os.getenv("L1SCP"))
-        printer_response = label_printer_connection(label_structures, printer_address, printer_port)
-        response += printer_response
-        response += "\n"
-        printer_address = os.getenv("L1CID")
-        printer_port = int(os.getenv("L1CP"))
-        printer_response = label_printer_connection(label_structures, printer_address, printer_port)
-        response += printer_response
-        response += "\n"
-        address = os.getenv("L2SID")
-        port = int(os.getenv("L2SP"))
-        printer_response = label_printer_connection(label_structures, printer_address, printer_port)
-        response += printer_response
-        response += "\n"
-
+        for printer in printers:
+            printer_ip = printer["ip"]
+            printer_port = printer["port"]
+            printer_response = label_printer_connection(box_label_structures, printer_ip, printer_port)
+            response += f"{printer_response}\n\r"
         return response
     except Exception as ex:
         print("Pallet label structure could not be uploaded due to: \n", ex)
