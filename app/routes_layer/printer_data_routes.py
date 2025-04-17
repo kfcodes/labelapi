89aed2b5da0d4e37ac7c84aa43ba7e69da2669e1
kfcodes <+kfcodes@users.noreply.github.com>
2025-04-17 21:38:37 +0100

    Added function to sync box label structures to printer sync route

--- app/routes_layer/printer_data_routes.py
+++ app/routes_layer/printer_data_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from data_controller_layer.printer_data_controller import *
 from data_controller_layer.pallet_label_controller import upload_pallet_label_data_to_printers
-# from data_controller_layer.box_label_controller import upload_box_label_data_to_printers
+from data_controller_layer.box_label_controller import upload_box_label_structures_to_printers
 from data_controller_layer.vars_controller import load_all_config_data
 
 printer_controller_router = APIRouter()
@@ -14,5 +14,6 @@ def startup():
 async def upload_label_structures_to_all_printers():
     all_printers = get_all_printer_connections()
     response = await upload_pallet_label_data_to_printers(all_printers)
-    # box_label_response = await upload_box_label_data_to_printers(all_printers)
+    box_label_response = await upload_box_label_structures_to_printers(all_printers)
     return response, box_label_response
+
