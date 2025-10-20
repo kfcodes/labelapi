545487f41424064c0277cef6ff5f077df53f5b6e
kfcodes <+kfcodes@users.noreply.github.com>
2025-10-20 18:01:59 +0100

    Added label related database functions to the database layer

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,11 +1,27 @@
 from .db_connection import db
+from .label_data_gateway import (a_get_brand_id_for_product,
+                                 a_get_label_context_for_brand,
+                                 a_get_required_eol_values,
+                                 get_barcode_formats_for_type,
+                                 get_brand_id_for_product,
+                                 get_finished_product_details,
+                                 get_required_fields_for_label)
+
+)
 from .read_db import read_db, read_to_list_index
 from .write_db import update_pallet_packing_list, write_db
 
 __all__ = [
+    "a_get_brand_id_for_product",
+    "a_get_label_context_for_brand",
+    "a_get_required_eol_values",
     "db",
     "read_db",
     "read_to_list_index",
     "write_db",
     "update_pallet_packing_list",
+    "get_barcode_formats_for_type",
+    "get_finished_product_details",
+    "get_brand_id_for_product",
+    "get_required_fields_for_label",
 ]
